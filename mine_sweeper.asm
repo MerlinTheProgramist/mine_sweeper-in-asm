3a6bd45f282e8b086cbd5a4056f77e1042fe5785
Merlin <merlin-1@o2.pl>
2023-05-27 18:25:39 +0200

    flagging cells

--- mine_sweeper.asm
+++ mine_sweeper.asm
@@ -34,7 +34,8 @@ extern print_num
 
 %define NREV_CHAR '#'
 %define BOMB_CHAR '*'
-%define EMPT_CHAR ' '
+%define FLAG_CHAR 'F'
+%define EMPT_CHAR '.'
 %define ENDL_CHAR 0ah
 
 %define ESC  0x1B
@@ -47,6 +48,8 @@ extern print_num
 %define HALF_QWORD, 4294967296
 
 generate_board:
+    mov BYTE[mines_left], 0
+
     get_rdtsc ; rax
     mov     rsi, 0
     _gen_board_for:
@@ -57,12 +60,13 @@ generate_board:
         
         ; if rax > HARF_QWORD => matrix[rsi] = BOMB 
         ; else => matrix[ris] = NOMINAL
-        cmp     rax, 1599903625
+        cmp     rax, 1000000000
         jg  _gen_greater
             mov     BYTE[matrix + rsi], NOMINAL
             jmp _gen_greater_skip
         _gen_greater:
         mov     BYTE[matrix + rsi], BOMB
+        inc     BYTE[mines_left]
         _gen_greater_skip:
 
         pop     rax
@@ -80,10 +84,10 @@ print_matrix:
 
     ; for y in 0..BOARD_SIZE
     xor     rsi, rsi
-    _pm_for_y:
+    .y:
         ; for x in 0..BOARD_SIZE
         xor     rdi, rdi
-        _pm_for_x:
+        .x:
             push rsi
             push rdi
 
@@ -106,17 +110,23 @@ print_matrix:
                 mov     r15, 1              ; mark a cursor :)
             _not_cursor:
             
+            test    al, FLAGGED                                            ; if is bomb
+            jz _not_flag                                                
+                mov     BYTE[print_buffer + r8], FLAG_CHAR              ; print_row_buffer[rdi] = BOMB_CHAR
+                jmp     _pm_for_continue                                ; continue
+            _not_flag:
+ 
 
             test   al, REVEALED                                         ; if is not revealed
             jnz _rev
-                mov     BYTE[print_buffer + r8], NREV_CHAR     ; print_row_buffer[rdi] = NREV_CHAR
+                mov     BYTE[print_buffer + r8], NREV_CHAR              ; print_row_buffer[rdi] = NREV_CHAR
                 jmp     _pm_for_continue                                ; continue
             _rev:
 
 
             test    al, BOMB                                            ; if is bomb
             jz _not_bomb                                                
-                mov     BYTE[print_buffer + r8], BOMB_CHAR   ; print_row_buffer[rdi] = BOMB_CHAR
+                mov     BYTE[print_buffer + r8], BOMB_CHAR              ; print_row_buffer[rdi] = BOMB_CHAR
                 jmp     _pm_for_continue                                ; continue
             _not_bomb:
 
@@ -125,7 +135,7 @@ print_matrix:
             jz _not_count
                 and     al, COUNT_MASK     
                 add     al, '1'
-                mov     BYTE[print_buffer + r8], al          ; print_row_buffer[rdi] = COUNT_MASK AND [`1`-`8`] 
+                mov     BYTE[print_buffer + r8], al                     ; print_row_buffer[rdi] = COUNT_MASK AND [`1`-`8`] 
                 jmp     _pm_for_continue                                ; continue
             _not_count:
             
@@ -150,24 +160,137 @@ print_matrix:
 
             inc    rdi
             cmp    rdi, BOARD_SIZE
-            jnz _pm_for_x    
+            jnz print_matrix.x    
 
         mov    BYTE[print_buffer+r8], ENDL_CHAR
         inc    r8               ; inc print_buffer_index because of ENDLINE
 
         inc    rsi
         cmp    rsi, BOARD_SIZE
-        jnz _pm_for_y
-
-
+        jnz print_matrix.y
     
 
     ; print rows buffer
     print  print_buffer, print_buffer_len
     ret
 
-uncover_action:
+game_over:
+
+    sys_exit
+ret
+
+; cl for x
+; dl for y
+; rax return
+is_pos_valid:
+    cmp cl, BOARD_SIZE
+    je  is_pos_valid.invalid
+    cmp cl, 0
+    jl  is_pos_valid.invalid
+
+    cmp dl, BOARD_SIZE
+    je  is_pos_valid.invalid
+    cmp dl, 0
+    jl  is_pos_valid.invalid
+
+    .valid
+    mov     rax, 1
+ret 
+    .invalid
+    xor     rax, rax
+ret
+
+; uncover tile on:
+; cl for x
+; dl for y
+; r8 for auto: 1 for auto
+uncover:
+    push rax
+    push rbx
+    push rcx
+    push rdx
+    push r8
     
+    call is_pos_valid ; if position not valid then return
+    test  rax, 1
+    jz uncover.nothing
+        
+
+    ; mov dh, [cursor_pos+Pos.x]
+
+    ; selected cell rax
+    xor    rax, rax
+    mov    al, cl
+    mov    rbx, BOARD_SIZE
+    mul    rbx ;ax = al * BOARD_SIZE
+    add    al, dl
+
+    mov    al, BYTE[matrix + rax]
+
+    ; do nothing when tile is uncovered, if either cursor or auto
+    test    al, REVEALED
+    jz uncover.continue
+        ret
+    .continue
+    
+    
+    test   al, BOMB ; if BOMB
+    jz     uncover.bomb
+    
+
+    ; if count: then stop uncovering more
+    test    al, HAS_COUNT
+    jnz     uncover.nothing
+
+    ; uncover all neighbors
+    mov r8, 1 ; as auto
+    
+    ; north
+    dec dl   
+    call uncover
+    ; east
+    inc  dl   
+    inc  cl   
+    call uncover
+    ; south
+    inc  dl
+    dec  cl   
+    call uncover
+    ; west
+    dec  dl 
+    dec  cl   
+    call uncover
+
+    .bomb:
+    test   r8, 1    ; if clicked
+    jnz     uncover.nothing ; if auto and bomb then dont show anything
+        call game_over ; then game over
+    
+
+    .nothing:
+    pop r8
+    pop rdx
+    pop rcx
+    pop rbx
+    pop rax    
+ret
+
+flag:
+    ; selected cell rax
+    xor    rax, rax
+    mov    al, [cursor_pos+Pos.y]
+    mov    bl, BOARD_SIZE
+    mul    bl ;ax = al * BOARD_SIZE
+    add    al, [cursor_pos+Pos.x]
+
+    test BYTE[matrix + rax], REVEALED
+    jnz flag.nothing
+
+    or   BYTE[matrix + rax], FLAGGED
+
+    flag.nothing:
+ret
+
 
 _start:
 
@@ -175,53 +298,72 @@ _start:
     ; mov     rax, [input_buffer]
     ; print input_buffer, 1
     ; call print_endl
+    
     call generate_board
+    mov  BYTE[matrix+1], FLAGGED
+    
+    mov  cl, [cursor_pos+Pos.y]
+    mov  dl, [cursor_pos+Pos.x]
+    xor  r8, r8
+    call uncover
 
     _game_loop:
         call print_matrix
 
+        input:
         call enable_key_read
-
         call sys_read
-        
-        ; call print_num
         call disable_key_read
 
-        xor     rdx, rdx
-        cmp     rax, 119
-        jnz _not_w
-            dec dl
-            jmp _read_end
-        _not_w:
-        cmp     rax, 's'
-        jnz _not_s
-            inc dl
-            jmp _read_end
-        _not_s:
-        cmp     rax, 'a'
-        jnz _not_a
-            dec dh
-            jmp _read_end
-        _not_a:
-        cmp     rax, 'd'
-        jnz _not_d
-            inc dh
-            jmp _read_end
-        _not_d:
-
-        _read_end:
+            xor     rdx, rdx
+            cmp     rax, 119
+            jnz input.w
+                dec dl
+                jmp input.end
+            .w:
+            cmp     rax, 's'
+            jnz input.s
+                inc dl
+                jmp input.end
+            .s:
+            cmp     rax, 'a'
+            jnz input.a
+                dec dh
+                jmp input.end
+            .a:
+            cmp     rax, 'd'
+            jnz input.d
+                inc dh
+                jmp input.end
+            .d:
+            cmp     rax, 'r'
+            jnz input.e
+                mov  cl, [cursor_pos+Pos.y]
+                mov  dl, [cursor_pos+Pos.x]
+                xor  r8, r8
+                call uncover
+                jmp input.end
+            .e:
+            cmp     rax, 'f'
+            jnz input.f
+                call flag
+                jmp input.end
+            .f:
+            ; if nothing from supported symboles was used 
+            jmp input   ; read input again
+            input.end:
 
         
         add dl, byte[cursor_pos+Pos.y]  ; temporary add
         add dh, byte[cursor_pos+Pos.x]  ; temporary add
-        ; if (y == SIZE or y<0) then dont save to mem
+        ; if (y == SIZE or y<0) then dont save y to mem
         cmp dl, BOARD_SIZE 
         je _dont_change_y
         cmp dl, 0
         jl _dont_change_y
             mov byte[cursor_pos+Pos.y], dl
         _dont_change_y:
-
+        ; if (x == SIZE or x<0) then dont save x to mem
         cmp dh, BOARD_SIZE 
         je _dont_change_x
         cmp dh, 0
@@ -229,7 +371,9 @@ _start:
             mov byte[cursor_pos+Pos.x], dh
         _dont_change_x:
         
-        
+
+
+
 
         call print_endl
     
@@ -251,8 +395,6 @@ endstruc
 section .data
 matrix: times BOARD_SIZE*BOARD_SIZE db 00100000b
 
-
-
 section .bss
 input_buffer: resb 1
 
@@ -264,5 +406,4 @@ print_buffer_len: equ $print_buffer
 cursor_pos: resb 2
 ; cursor_blink: resb 1
 
-
-
+mines_left: resb 1
